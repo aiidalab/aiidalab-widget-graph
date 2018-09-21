baa3c8093b8432af78f101bab3a19945cf78189f
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2018-09-22 00:01:50 +0200

    Various improvements to the widget.

--- aiidalab_widget_graph/graph.py
+++ aiidalab_widget_graph/graph.py
@@ -1,5 +1,5 @@
 import ipywidgets as widgets
-from traitlets import Unicode, List
+from traitlets import Unicode, List, Dict
 
 @widgets.register
 class GraphWidget(widgets.DOMWidget):
@@ -10,5 +10,6 @@ class GraphWidget(widgets.DOMWidget):
     _model_module = Unicode('aiidalab-widget-graph').tag(sync=True)
     _view_module_version = Unicode('^0.1.0').tag(sync=True)
     _model_module_version = Unicode('^0.1.0').tag(sync=True)
-    nodes = List([1,2,3]).tag(sync=True)
-    edges = List([[1,2],[2,3]]).tag(sync=True)
+    _graph = Dict().tag(sync=True)
+    #nodes = List([1,2,3]).tag(sync=True)
+    #edges = List([[1,2],[2,3]]).tag(sync=True)
--- js/lib/graph.js
+++ js/lib/graph.js
@@ -2,23 +2,7 @@ var widgets = require('@jupyter-widgets/base');
 var _ = require('lodash');
 var d3 = require('d3');
 
-window.$d3 = d3;
-
-// Custom Model. Custom widgets models must at least provide default values
-// for model attributes, including
-//
-//  - `_view_name`
-//  - `_view_module`
-//  - `_view_module_version`
-//
-//  - `_model_name`
-//  - `_model_module`
-//  - `_model_module_version`
-//
-//  when different from the base class.
-
-// When serialiazing the entire widget state for embedding, only values that
-// differ from the defaults will be specified.
+// The model
 var GraphModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
         _model_name : 'GraphModel',
@@ -27,42 +11,44 @@ var GraphModel = widgets.DOMWidgetModel.extend({
         _view_module : 'aiidalab-widget-graph',
         _model_module_version : '0.1.0',
         _view_module_version : '0.1.0',
-        nodes : [],
-        edges: []
-    })
+    }),
 });
 
 
 // Custom View. Renders the widget model.
 var GraphView = widgets.DOMWidgetView.extend({
+    nodeProxy: function() {
+        var nodes = this.model.get('_graph')['nodes'] || {};
 
-    nodeWrapper: function(nodes) {return _.map(nodes, function(nodeId) {
-        return {
-            "id": nodeId,
-            "group": nodeId,
-            };
-        })},
+        nodes = _.chain(nodes)
+            .toPairs().sortBy(0) // convert to [[k, v], [k, v], ...]
+            .map(function(pair) {return {"id": +pair[0], "meta": pair[1]}})
+            .value();
+        
+        return nodes;
+        },
 
-    edgeWrapper: function(edges) {return _.map(edges, function(edge) {
+    linkProxy: function() {
+        var links = this.model.get('_graph')['links'] || [];
+
+        return _.map(links, function(link) {
         return {
-            "source": edge[0], 
-            "target": edge[1], 
-            "value": 1
+            "source": link[0], 
+            "target": link[1], 
+            "meta": link[2] || {}
             };
         })},
 
     ticked: function(d3this) {
-        //this.d3Simulation.stop();
         this.d3Link
             .attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
-    
+
         this.d3Node
             .attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
-        
         },
 
     dragstarted: function(d) {
@@ -82,7 +68,7 @@ var GraphView = widgets.DOMWidgetView.extend({
             d.fy = null;
           },
 
-    updateGraph: function(nodeData, edgeData) {
+    updateGraph: function(nodeData, linkData) {
         var _this = this;
 
         var colorFunction = function(groupId) {
@@ -91,35 +77,41 @@ var GraphView = widgets.DOMWidgetView.extend({
             .range(d3.schemeCategory10)(groupId);
         };
 
-        // Stop it first; prevent to enter infinite loops if there is a link pointing to unknown nodes
+        // Stop it first; prevent to enter infinite loops if there is a 
+        // link pointing to unknown nodes.
         // Note that this is still not enough to prevent problems
         this.d3Simulation.stop();
 
-        this.d3Link = this.d3Link.data(edgeData, function(d) { return d.source.id + "-" + d.target.id; });
+        // Put links below nodes so the node is above
+        this.d3Link = this.d3Link.data(linkData, function(d) { 
+            return d.source.id + "-" + d.target.id; 
+        });
         this.d3Link.exit().remove();
         this.d3Link = this.d3Link
             .enter().append("line").merge(this.d3Link)
-            .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
+            .attr("stroke-width", function(d) { return Math.sqrt(d.meta.value || 1); });
 
         this.d3Node = this.d3Node.data(nodeData, function(d) { return d.id;});
         this.d3Node.exit().remove();
         this.d3Node = this.d3Node
             .enter().append("circle").merge(this.d3Node)
             .attr("r", 5)
-            .attr("fill", function(d) { return colorFunction(d.group); })
+            .attr("fill", function(d) { return colorFunction(d.meta.group || d.id); })
             .call(d3.drag()
             .on("start", function(d,i) { _this.dragstarted(d,i,this); })
             .on("drag", function(d,i) { _this.dragged(d,i,this); })
             .on("end", function(d,i) { _this.dragended(d,i,this); }));
-        
+    
         //this.d3Node.append("title")
         //   .text(function(d) { return d.id; });
 
         this.d3Simulation
             .nodes(nodeData);
         this.d3Simulation.force("link")
-          .links(edgeData); 
-        this.d3Simulation.alpha(1).restart();
+          .links(linkData); 
+        this.d3Simulation
+        //.alpha(1)
+        .restart();
     },
 
     render: function() {
@@ -127,15 +119,13 @@ var GraphView = widgets.DOMWidgetView.extend({
         var theHeight = 400;
         var _this = this;
 
-        // Maybe this can be removed
+        // Optional
         this.elId = 'graph-'+this.model.cid;
         this.el.id = this.elId;
-        // Up to here
 
         var svgTag = '<svg id width="' + theWidth + '" height="' + theHeight + '"></svg>';
         this.el.innerHTML = svgTag;
         this.d3Elem = d3.select(this.el).select('svg');
-        //console.log('render', this.el, this.d3Elem);
         
         this.d3Simulation = d3.forceSimulation()
           .force("link", d3.forceLink().id(function(d) { return d.id; }))
@@ -144,42 +134,41 @@ var GraphView = widgets.DOMWidgetView.extend({
           .alphaTarget(1)
           .on("tick", function() { _this.ticked(this); }); // In this way 'this' inside the function is the View and not the d3 selection, and we pass the d3 selection as a parameter if needed
 
-        var edgeData = this.edgeWrapper(this.model.get('edges'));
+        var nodeData = this.nodeProxy();
+        this.d3Node = this.d3Elem.append("g")
+              .attr("class", "nodes")
+              .selectAll("circle");
+
+        var linkData = this.linkProxy();
         this.d3Link = this.d3Elem.append("g")
             .attr("class", "links")
             .selectAll("line");
 
-        var nodeData = this.nodeWrapper(this.model.get('nodes'));
-        this.d3Node = this.d3Elem.append("g")
-            .attr("class", "nodes")
-            .selectAll("circle");
+        this.updateGraph(nodeData, linkData);
 
-        this.updateGraph(nodeData, edgeData);
-
-
-        this.model.on('change:nodes change:edges', this.graph_changed, this);
+        this.model.on('change:_graph', this.graph_changed, this);
     },
 
     graph_changed: function() {
-        var edges = this.edgeWrapper(this.model.get('edges'));
-        var nodes = this.nodeWrapper(this.model.get('nodes'));
-
-        console.log(edges, nodes);
-        this.updateGraph(nodes, edges);
-
-        //this.d3Link.attr('d', edges);
-        //this.d3Node.attr('d', nodes);
+        var nodes = this.nodeProxy();
+        var links = this.linkProxy();
+        this.updateGraph(nodes, links);
     }
 });
 
 // TODO:
-// - Make sure that when nodes are removed, also the corresponding edges are removed
-//   (and they should be removed first!!) Otherwise it crashes in JS
-// - Make a single call to change edges and nodes at the same time? Maybe provide
-//   a Python Object on the other side?
-// - Change the data format so that we can fix the group/color and the weight of links from python
-// - rename link/edge, and put nodes always first
-// - set default graph in python to be empty
+// - Make sure that when nodes are removed, also the corresponding links are removed
+//   (and they should be removed first!!) Otherwise it crashes in JS (i.e.: validate
+//   the nodes in python, and make a guard in JS to avoid problems)
+// - Povide a Python Object on the other side?
+// - Instead of making a color group, pass a color string from python; expose
+//   colorbrewer2 in python
+// - fix the color function (there is now a 'range(100)')
+// - make the_width, the_height as parameters from python
+// - check parameters (e.g. when there is a node not linked to anything)
+// - check if it is correct that the points seem to move when a new node is added
+// - I think it's not needed that node keys are integers - verify, and remove the +
+//   in JS in front of the value to cast it.
 
 module.exports = {
     GraphModel : GraphModel,
